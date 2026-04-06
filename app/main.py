59bfb5a6409b966d6073f306af7de2583dc714a9
leandro <l1d9l9a4@gmail.com>
2026-04-06 20:12:34 +0200

    connecting everything in main

--- app/main.py
+++ app/main.py
@@ -2,11 +2,15 @@ from fastapi import FastAPI
 from app.database import engine, Base
 
 from app.models import user, invoice
+from app.routes import auth, invoice
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+app.include_router(auth.router)
+app.include_router(invoice.router)
+
 @app.get("/")
 def root():
-    return {"message": "API Facturacion funcionando y tablas creadas"}
+    return {"message": "API de Facturación funcionando"}
