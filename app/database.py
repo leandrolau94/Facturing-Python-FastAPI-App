c00f9dbd1de700e0c6ae0216e8eea6183436d2f4
leandro <l1d9l9a4@gmail.com>
2026-04-06 17:37:04 +0200

    set up FastAPI with PostgreSQL and Project Structure

--- app/database.py
+++ app/database.py
@@ -3,9 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
-load_dotenv()
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+env_path = os.path.join(BASE_DIR, ".env")
+
+load_dotenv(dotenv_path=env_path)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+print("DATABASE_URL:", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 
